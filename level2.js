beef786722bc33bc5d3a1f10ad69a939652702e7
Glitch (glitch-hello-website) <none>
2022-04-02 23:47:11 +0000

    ⛄️🚵 Checkpoint ./level1.js:40905407/6101 ./style.css:40905407/9976 ./level1.html:40905407/7260 ./.gitignore:40905407/12 ./level2.html:40905407/21794 ./level2.js:40905407/9769 ./level3.html:40905407/9268

---
+++ level2.js
@@ -0,0 +1,105 @@
+var counter = 0;
+var number_cards = 6;
+var attemptLeft = number_cards - 1;
+var score = 0;
+
+document.getElementById("attemptDiv").innerHTML =
+  "MOVES LEFT" + "<br>" + attemptLeft + "/" + (number_cards - 1);
+document.getElementById("score").innerHTML = "SCORE" + "<br>" + score;
+
+function attempt() {
+  attemptLeft--;
+  document.getElementById("attemptDiv").innerHTML =
+    "MOVES LEFT" + "<br>" + attemptLeft + "/" + (number_cards - 1);
+}
+
+function winner_loser() {
+  if (attemptLeft == 0) {
+    document.getElementById("status").innerHTML = "You Lost!";
+    score--;
+    document.getElementById("score").innerHTML = "SCORE" + "<br>" + score;
+    attemptLeft = number_cards - 1;
+    document.getElementById("attemptDiv").innerHTML =
+      "MOVES LEFT" + "<br>" + attemptLeft + "/" + (number_cards - 1);
+  } else if (counter == number_cards) {
+    document.getElementById("status").innerHTML = "You Won!";
+    score++;
+    document.getElementById("score").innerHTML = "SCORE" + "<br>" + score;
+  }
+}
+
+const cards = document.querySelectorAll(".cards");
+
+let FlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!FlippedCard) {
+    FlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  lockBoard = true;
+
+  checkForMatch();
+}
+
+
+function checkForMatch() {
+  if (
+    firstCard.getAttribute("data-framework") ===
+    secondCard.getAttribute("data-framework")
+  ) {
+    disableCards();
+    return;
+  }
+  console.log("about to unflip");
+  unflipCards();
+}
+
+function disableCards() {
+  console.log("in disable");
+  counter += 2;
+
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  winner_loser();
+  resetBoard();
+}
+
+function unflipCards() {
+  console.log("in unflip");
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1500);
+
+  attempt();
+  winner_loser();
+}
+
+function resetBoard() {
+  [FlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+// shuffle elements
+cards.forEach((card) => {
+  const ramdomPos = Math.floor(Math.random() * cards.length);
+  card.style.order = ramdomPos;
+});
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
